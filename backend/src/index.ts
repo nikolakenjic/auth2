49c35cb5071d9316d0184099f0bb454bcefe3c02
Nikola Kenjic <nikola.nico89@gmail.com>
2025-07-03 23:34:34 +0200

    add first initial route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('hello')
+})
+
 app.listen(3500, () => {
     console.log("Server started on port 3500");
 })

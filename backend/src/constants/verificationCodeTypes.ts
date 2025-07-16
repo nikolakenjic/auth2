ecf594ad31415db3bb4a18173cdba423c26b5be9
Nikola Kenjic <nikola.nico89@gmail.com>
2025-07-16 19:30:17 +0200

    change backend name to server

--- backend/src/constants/verificationCodeTypes.ts
+++
@@ -1,6 +0,0 @@
-const enum VerificationCodeTypes {
-  EmailVerification = 'email_verification',
-  PasswordReset = 'password_reset',
-}
-
-export default VerificationCodeTypes;
--- backend/src/routes/auth.route.ts
+++
@@ -1,8 +0,0 @@
-import {Router} from "express";
-import {registerHandler} from "../controllers/auth.controller";
-
-const router = Router();
-
-router.post('/register', registerHandler);
-
-export default router

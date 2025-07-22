3d332b52b17021e13cb3776797bf6abe2aa9fa96
Nikola Kenjic <nikola.nico89@gmail.com>
2025-07-22 23:04:19 +0200

    create session model

---
+++ server/src/models/session.model.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import {thirtyDaysFromNow} from "../utils/date";
+
+export interface SessionDocument extends mongoose.Document {
+    userId: mongoose.Types.ObjectId;
+    createdAt: Date;
+    expiresAt: Date;
+}
+
+const sessionSchema = new mongoose.Schema<SessionDocument>({
+    userId: {
+        ref: "User",
+        type: mongoose.Schema.Types.ObjectId,
+        index: true,
+    },
+    createdAt: {type: Date, required: true, default: Date.now()},
+    expiresAt: {type: Date, default: thirtyDaysFromNow},
+})
+
+const sessionModel = mongoose.model<SessionDocument>("Session", sessionSchema);
+
+export default sessionModel;

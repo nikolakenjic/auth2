d9a3ec11cc711a2d91fa807d3ab2e438c01e94b3
Nikola Kenjic <nikola.nico89@gmail.com>
2025-07-29 22:47:16 +0200

    app error

---
+++ server/src/utils/AppError.ts
@@ -0,0 +1,14 @@
+import {HttpStatusCode} from "../constants/http";
+import AppErrorCode from "../constants/appErrorCode";
+
+class AppError extends Error {
+    constructor(
+        public statusCode: HttpStatusCode,
+        public message: string,
+        public errorCode?: AppErrorCode
+    ) {
+        super(message)
+    }
+}
+
+export default AppError

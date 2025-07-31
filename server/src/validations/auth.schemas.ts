06d2f3293a282502042e7f381958f88e11a73ed6
Nikola Kenjic <nikola.nico89@gmail.com>
2025-07-31 23:16:18 +0200

    auth schemas

---
+++ server/src/validations/auth.schemas.ts
@@ -0,0 +1,16 @@
+import {z} from "zod";
+
+const emailSchema = z.string().email().min(1).max(50)
+const passwordSchema = z.string().min(6).max(50)
+
+export const loginSchema = z.object({
+    email: emailSchema,
+    password: passwordSchema,
+})
+
+export const registerSchema = loginSchema.extend({
+    confirmPassword: z.string().min(6).max(50),
+}).refine((data) => data.password === data.confirmPassword, {
+    message: 'Passwords do not match',
+    path: ['confirmPassword'],
+})

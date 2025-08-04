bbdb172add96d7e512de8f814bcbc968f0640958
Nikola Kenjic <nikola.nico89@gmail.com>
2025-08-04 23:10:58 +0200

    logout route and controller

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,8 +1,10 @@
 import catchAsync from "../utils/catchAsync";
 import {createAccount, loginUser} from "../services/auth.service";
 import {CREATED, OK} from "../constants/http";
-import {setAuthCookies} from "../utils/cookies";
+import {clearAuthCookies, setAuthCookies} from "../utils/cookies";
 import {loginSchema, registerSchema} from "../validations/auth.schemas";
+import {verifyToken} from "../utils/jwt";
+import SessionModel from "../models/session.model";
 
 export const registerHandler = catchAsync(async (req, res, next) => {
 // //    validate request
@@ -30,5 +32,19 @@ export const loginHandler = catchAsync(async (req, res, next) => {
     return setAuthCookies({res, accessToken, refreshToken}).status(OK).json({
         status: 'success',
         user
+    })
+})
+
+export const logoutHandler = catchAsync(async (req, res, next) => {
+    const accessToken = req.cookies.accessToken
+    const {payload} = verifyToken(accessToken)
+
+    if(payload) {
+        await SessionModel.findByIdAndDelete(payload.sessionId)
+    }
+
+    return clearAuthCookies(res).status(OK).json({
+        status: 'success',
+        message: 'Logged out',
     })
 })
--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -1,9 +1,10 @@
 import {Router} from "express";
-import {loginHandler, registerHandler} from "../controllers/auth.controller";
+import {loginHandler, logoutHandler, registerHandler} from "../controllers/auth.controller";
 
 const router = Router();
 
 router.post('/register', registerHandler);
 router.post('/login', loginHandler)
+router.get('/logout', logoutHandler)
 
 export default router

f1d15a13b0d371879981637d2e76573168bc72e5
Nikola Kenjic <nikola.nico89@gmail.com>
2025-08-08 23:45:19 +0200

    dynamic email to and from

--- server/src/constants/env.ts
+++ server/src/constants/env.ts
@@ -9,13 +9,13 @@ const getEnv = (key: string, defaultValue?: string): string => {
     return value;
 };
 
-// export const NODE_ENV = getEnv('NODE_ENV', 'development');
+export const NODE_ENV = getEnv('NODE_ENV', 'development');
 export const PORT = getEnv('PORT', '3500');
 export const MONGO_URI = getEnv('MONGO_URI');
 export const APP_ORIGIN = getEnv('APP_ORIGIN');
 export const JWT_SECRET = getEnv('JWT_SECRET');
 export const JWT_REFRESH_SECRET = getEnv('JWT_REFRESH_SECRET');
-// export const EMAIL_SENDER = getEnv('EMAIL_SENDER');
+export const EMAIL_SENDER = getEnv('EMAIL_SENDER');
 export const RESEND_API_KEY = getEnv('RESEND_API_KEY');
 
 export const ENV = {
--- server/src/utils/sendMail.ts
+++ server/src/utils/sendMail.ts
@@ -1,4 +1,5 @@
 import resend from "../config/resend";
+import {EMAIL_SENDER, ENV, NODE_ENV} from "../constants/env";
 
 type Params = {
     to: string;
@@ -7,10 +8,16 @@ type Params = {
     html: string;
 }
 
+const getEmailFrom = () =>
+    NODE_ENV === ENV.DEVELOPMENT ? 'onboarding@resend.dev' : EMAIL_SENDER
+
+const getEmailTo = (to: string) =>
+    NODE_ENV === ENV.DEVELOPMENT ? 'delivered@resend.dev' : to
+
 export const sendMail = async ({to, subject, text, html}: Params) =>
     await resend.emails.send({
-        from: 'onboarding@resend.dev',
-        to: "delivered@resend.dev",
+        from: getEmailFrom(),
+        to: getEmailTo(to),
         subject,
         text,
         html

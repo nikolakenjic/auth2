7bf830331a2f771ae4c14f9a8ab1fc0303ef6ba0
Nikola Kenjic <nikola.nico89@gmail.com>
2025-08-18 20:51:28 +0200

    fix session controller to use guaranteed userId and sessionId

--- server/src/controllers/session.controller.ts
+++ server/src/controllers/session.controller.ts
@@ -1,18 +1,18 @@
 import catchAsync from '../utils/catchAsync';
-import {NOT_FOUND, OK} from '../constants/http';
+import {OK} from '../constants/http';
 import {
     deleteUserSession,
     getUserSessions,
 } from '../services/session.service';
 
 export const getSessionsHandler = catchAsync(async (req, res, next) => {
-    const sessions = await getUserSessions(req.userId, req.sessionId);
+    const sessions = await getUserSessions(req.userId!, req.sessionId);
 
     return res.status(OK).json(sessions);
 });
 
 export const deleteSessionHandler = catchAsync(async (req, res, next) => {
-    await deleteUserSession(req.sessionId, req.userId);
+    await deleteUserSession(req.sessionId, req.userId!);
 
     return res.status(OK).json({
         status: 'success',

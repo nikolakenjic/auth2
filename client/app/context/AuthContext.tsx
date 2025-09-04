7ddf917bdca2100743017469cacb80d6667a0e37
Nikola Kenjic <nikola.nico89@gmail.com>
2025-09-05 00:00:42 +0200

    auth context

--- client/app/context/AuthContext.tsx
+++ client/app/context/AuthContext.tsx
@@ -1,36 +1,58 @@
-import React, {createContext, useContext, useState, useEffect, ReactNode} from "react"
-import AuthService from "@/app/services/api-client/auth.service";
+import React, {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  ReactNode,
+} from 'react';
+import AuthService from '@/app/services/api-client/auth.service';
 
 interface User {
-    _id: string
-    email: string
+  email: string;
+  password: string;
 }
 
 interface AuthContextType {
-    user: User | null
-    login: (data: any) => Promise<void>
-    logout: () => Promise<void>
+  user: User | null;
+  login: (data: any) => Promise<void>;
+  logout: () => Promise<void>;
 }
 
-const AuthContext = createContext<AuthContextType | undefined>(undefined)
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
-export const AuthProvider = ({children}: { children: ReactNode }) => {
-    const [user, setUser] = useState<User | null>(null)
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
 
-    // when the app started, call backend to see if user still login
-    useEffect(() => {
-        const fetchCurrentUser = async () => {
-            try {
-            // To do: Fetch current User
-            } catch {
-                setUser(null);
-            }
-        }
-    }, []);
+  // when the app started, call backend to see if user still login
+  useEffect(() => {
+    const fetchCurrentUser = async () => {
+      try {
+        // To do: Fetch current User
+      } catch {
+        setUser(null);
+      }
+    };
+  }, []);
 
-    const login = async(data: any) => {
-        const res = await AuthService.login<{user: User}>(data)
-        setUser(res.email)
-    }
+  const login = async (data: any) => {
+    const res = await AuthService.login(data);
+    setUser(res);
+  };
 
-}
+  const logout = async () => {
+    await AuthService.logout();
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const ctx = useContext(AuthContext);
+  if (!ctx) throw new Error('useAuth must be used within a AuthProvider');
+  return ctx;
+};

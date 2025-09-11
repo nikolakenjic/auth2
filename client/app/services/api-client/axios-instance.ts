ce6034b60317735eba1add5ded9a05c46d38d7bb
Nikola Kenjic <nikola.nico89@gmail.com>
2025-09-11 23:14:43 +0200

    reduce axios

--- client/app/services/api-client/axios-instance.ts
+++ client/app/services/api-client/axios-instance.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-
 const axiosInstance = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3500",
     headers: {
@@ -8,28 +7,5 @@ const axiosInstance = axios.create({
     },
     withCredentials: true,
 });
-
-
-axiosInstance.interceptors.request.use(
-    (config) => {
-
-        const token = localStorage.getItem("accessToken");
-        if (token) {
-            config.headers!["Authorization"] = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => Promise.reject(error)
-);
-
-
-axiosInstance.interceptors.response.use(
-    (response) => response,
-    (error) => {
-
-        console.error(error);
-        return Promise.reject(error);
-    }
-);
 
 export default axiosInstance;

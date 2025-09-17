5df1eaceed8b59be068017de7f825bf0a0bacd6d
Nikola Kenjic <nikola.nico89@gmail.com>
2025-09-17 22:09:34 +0200

    add endpoint for update and fetch

--- client/app/services/base.service.ts
+++ client/app/services/base.service.ts
@@ -5,11 +5,15 @@ export default class BaseService {
         return ApiClient.post(endpoint, body, queryParams).then(res => res.data);
     }
 
+    static async fetch<T>(endpoint: string, queryParams = {}, config = {}): Promise<T> {
+        return ApiClient.get(endpoint, queryParams, config).then(res => res.data);
+    }
+
     static async delete<T>(endpoint: string, queryParams = {}): Promise<T> {
         return ApiClient.remove(endpoint, queryParams).then(res => res.data);
     }
 
-    static async fetch<T>(endpoint: string, queryParams = {}, config = {}): Promise<T> {
-        return ApiClient.get(endpoint, queryParams, config).then(res => res.data);
+    static async update<T>(endpoint: string, body: any, queryParams = {}): Promise<T> {
+        return ApiClient.put(endpoint, body, queryParams).then(res => res.data);
     }
 }

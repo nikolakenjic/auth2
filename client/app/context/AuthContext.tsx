ccda4dc8a4882b26b423306332e54a2d5b6ba12d
Nikola Kenjic <nikola.nico89@gmail.com>
2025-09-23 20:40:34 +0200

    get user profile on refresh or initial loading

--- client/app/context/AuthContext.tsx
+++ client/app/context/AuthContext.tsx
@@ -1,13 +1,33 @@
 'use client'
 
-import { createContext, useContext, useState } from "react";
+import {createContext, useContext, useEffect, useState} from "react";
 import {AuthContextType, User} from "@/app/types/auth";
 import AuthService from "@/app/services/api-client/auth.service";
+import UserService from "@/app/services/user.service";
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
-export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+export const AuthProvider = ({children}: { children: React.ReactNode }) => {
     const [user, setUser] = useState<User | null>(null);
+    const [loading, setLoading] = useState(true);
+
+    // Fetch user on app start
+    const refreshUser = async () => {
+        try {
+            setLoading(true);
+            const response = await UserService.getMe()
+            setUser(response.user);
+        } catch (err) {
+            console.error(err);
+            setUser(null);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        refreshUser()
+    }, []);
 
     // login
     const login = async (credentials: any) => {
@@ -44,6 +64,7 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
         return AuthService.resetPassword(payload);
     };
 
+
     const value: AuthContextType = {
         user,
         login,
@@ -52,6 +73,7 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
         verifyEmail,
         sendPasswordReset,
         resetPassword,
+        loading
     };
 
     return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;

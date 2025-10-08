ae2a7bf697ccaa6141beac2e5955c9a79b0f2047
Nikola Kenjic <nikola.nico89@gmail.com>
2025-10-08 23:34:58 +0200

    add interceptors for create new access token

--- client/app/services/api-client/axios-instance.ts
+++ client/app/services/api-client/axios-instance.ts
@@ -1,4 +1,9 @@
-import axios from "axios";
+import axios, {AxiosRequestConfig} from "axios";
+import AuthService from "@/app/services/api-client/auth.service";
+
+interface AxiosRequestConfigWithRetry extends AxiosRequestConfig {
+    _retry?: boolean;
+}
 
 const axiosInstance = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3500",
@@ -7,5 +12,32 @@ const axiosInstance = axios.create({
     },
     withCredentials: true,
 });
+
+// Interceptor for auto refresh token
+axiosInstance.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config as AxiosRequestConfigWithRetry;
+
+    //     If token expires and we do not try refresh
+        if(error.response?.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+
+            try {
+            //     Call backend to refresh accessToken
+                await AuthService.refresh()
+
+            //     send again original request
+                return axiosInstance(originalRequest)
+            } catch (refreshError) {
+                console.error('RefreshToken Invalid or Expired')
+
+                return Promise.reject(refreshError);
+            }
+        }
+
+        return Promise.reject(error);
+    }
+)
 
 export default axiosInstance;

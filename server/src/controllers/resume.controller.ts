b5b90c48329d9f731720b8dd26e47d87b0679be6
Nikola Kenjic <nikola.nico89@gmail.com>
2025-11-20 23:46:36 +0100

    create and get all resumes controls and service

--- server/src/controllers/resume.controller.ts
+++ server/src/controllers/resume.controller.ts
@@ -1,12 +1,29 @@
 import catchAsync from "../utils/catchAsync";
+import {createResumeService, getAllResumesService} from "../services/resume.service";
+import {CREATED, OK} from "../constants/http";
+import {createResumeSchema} from "../validations/resume.schemas";
 
 
 export const getAllResumes = catchAsync(async (req, res, next) => {
-    res.send('all resumes')
+    const userId = req.userId
+
+    const resumes = await getAllResumesService(userId);
+
+    return res.status(OK).json({
+        status: "success",
+        resumes
+    })
 })
 
 export const createResume = catchAsync(async (req, res, next) => {
-    res.send('create resume')
+    const data = createResumeSchema.parse(req.body)
+
+    const resume = await createResumeService(req.userId, data)
+
+    return res.status(CREATED).json({
+        status: "success",
+        resume
+    })
 })
 
 export const getResumeById = catchAsync(async (req, res, next) => {
--- server/src/services/resume.service.ts
+++ server/src/services/resume.service.ts
@@ -4,3 +4,10 @@ import ResumeModel from "../models/resume.model";
 export const getAllResumesService = async (userId: mongoose.Types.ObjectId) => {
     return await ResumeModel.find({userId})
 }
+
+export const createResumeService = async (userId: mongoose.Types.ObjectId, data: any) => {
+    return await ResumeModel.create({
+        ...data,
+        userId
+    })
+}

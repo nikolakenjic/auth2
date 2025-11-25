0d6f4ac685b5c5a5814f1cda085958638b0278d1
Nikola Kenjic <nikola.nico89@gmail.com>
2025-11-25 23:03:56 +0100

    create resume services

--- server/src/services/resume.service.ts
+++ server/src/services/resume.service.ts
@@ -11,3 +11,19 @@ export const createResumeService = async (userId: mongoose.Types.ObjectId, data:
         userId
     })
 }
+
+export const getResumeByIdService = async (userId: mongoose.Types.ObjectId, resumeId: mongoose.Types.ObjectId) => {
+    return await ResumeModel.findOne({_id: resumeId, userId})
+}
+
+export const updateResumeService = async (userId: mongoose.Types.ObjectId, resumeId: mongoose.Types.ObjectId, data: any) => {
+    return await ResumeModel.findOneAndUpdate(
+        {_id: resumeId, userId},
+        {$set: data},
+        {new: true}
+    )
+}
+
+export const deleteResumeService = async (userId: mongoose.Types.ObjectId, resumeId: mongoose.Types.ObjectId) => {
+    return await ResumeModel.findOneAndDelete({_id: resumeId, userId})
+}

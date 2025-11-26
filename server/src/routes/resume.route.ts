116397d476d0f7832b47ffce03fba68a54ec3662
Nikola Kenjic <nikola.nico89@gmail.com>
2025-11-26 22:56:15 +0100

    refactor resume controller

--- server/src/routes/resume.route.ts
+++ server/src/routes/resume.route.ts
@@ -1,12 +1,14 @@
 import {Router} from 'express';
 import {createResume, deleteResume, getAllResumes, getResumeById, updateResume} from "../controllers/resume.controller";
+import {validate} from "../middleware/validate";
+import {createResumeSchema, updateResumeSchema} from "../validations/resume.schemas";
 
 const router = Router();
 
 router.get('/', getAllResumes)
-router.post('/', createResume)
+router.post('/', validate(createResumeSchema), createResume)
 router.get('/:id', getResumeById)
-router.patch('/:id', updateResume)
+router.patch('/:id',validate(updateResumeSchema), updateResume)
 router.delete('/:id', deleteResume)
 
 export default router;

7e3a2241e69a9f1b5e775cbff5f6d38921be3c40
Nikola Kenjic <nikola.nico89@gmail.com>
2025-11-26 22:57:00 +0100

    update resume controllers

--- server/src/controllers/resume.controller.ts
+++ server/src/controllers/resume.controller.ts
@@ -6,14 +6,12 @@ import {
     updateResumeService
 } from "../services/resume.service";
 import {CREATED, OK} from "../constants/http";
-import {createResumeSchema} from "../validations/resume.schemas";
-import mongoose from "mongoose";
+import {createResumeSchema, updateResumeSchema} from "../validations/resume.schemas";
+import {ensureFound, toObjectId} from "../utils/dataHelpers";
 
 
 export const getAllResumes = catchAsync(async (req, res, next) => {
-    const userId = req.userId
-
-    const resumes = await getAllResumesService(userId);
+    const resumes = await getAllResumesService(req.userId);
 
     return res.status(OK).json({
         status: "success",
@@ -33,10 +31,9 @@ export const createResume = catchAsync(async (req, res, next) => {
 })
 
 export const getResumeById = catchAsync(async (req, res, next) => {
-    const userId = req.userId
-    const resumeId = new mongoose.Types.ObjectId(req.params.id)
+    const resumeId = toObjectId(req.params.id)
 
-    const resume = await getResumeByIdService(userId, resumeId)
+    const resume = await getResumeByIdService(req.userId, resumeId)
 
     return res.status(OK).json({
         status: "success",
@@ -45,23 +42,22 @@ export const getResumeById = catchAsync(async (req, res, next) => {
 })
 
 export const updateResume = catchAsync(async (req, res, next) => {
-    const userId = req.userId
-    const resumeId = new mongoose.Types.ObjectId(req.params.id)
-    const data = req.body
+    const resumeId = toObjectId(req.params.id)
+    const data = updateResumeSchema.parse(req.body)
 
-    const updateResume = await updateResumeService(userId, resumeId, data)
+    const updatedResume = await updateResumeService(req.userId, resumeId, data)
+    ensureFound(updatedResume, 'Resume not found')
 
     return res.status(OK).json({
         status: "success",
-        resume: updateResume
+        resume: updatedResume
     })
 })
 
 export const deleteResume = catchAsync(async (req, res, next) => {
-    const userId = req.userId
-    const resumeId = new mongoose.Types.ObjectId(req.params.id)
+    const resumeId = toObjectId(req.params.id)
 
-    await deleteResumeService(userId, resumeId)
+    await deleteResumeService(req.userId, resumeId)
 
     return res.status(OK).json({
         status: "success",

2e6ecd03196e1897d13c6b0324bc98ddc2ecebf8
Nikola Kenjic <nikola.nico89@gmail.com>
2025-11-28 23:12:20 +0100

    feat(cover-letter-service:implement CRUD operations

--- server/src/services/coverLetter.service.ts
+++ server/src/services/coverLetter.service.ts
@@ -11,3 +11,28 @@ export const createCoverLetterService = async (userId: mongoose.Types.ObjectId, 
         userId
     })
 }
+
+export const getCoverLetterByIdService = async (userId: mongoose.Types.ObjectId, coverLetterId: mongoose.Types.ObjectId) => {
+    return await CoverLetterModel.findOne({_id: coverLetterId, userId})
+}
+
+export const updateCoverLetterService = async (
+    userId: mongoose.Types.ObjectId,
+    coverLetterId: mongoose.Types.ObjectId,
+    data: any
+) => {
+    return await CoverLetterModel.findOneAndUpdate(
+        {_id: coverLetterId, userId},
+        {$set: data},
+        {new: true}
+    )
+}
+
+export const deleteCoverLetterService = async (
+    userId: mongoose.Types.ObjectId,
+    coverLetterId: mongoose.Types.ObjectId
+) => {
+    return await CoverLetterModel.findOneAndDelete(
+        {_id: coverLetterId, userId},
+    )
+}

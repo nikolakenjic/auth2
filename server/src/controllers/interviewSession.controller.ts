ada6eaf3fe5a54b56c4aaf4297bc171fe70a7c92
Nikola Kenjic <nikola.nico89@gmail.com>
2025-11-30 23:00:24 +0100

    feat(interview-session-controller): add controller logic for get all, create and get by id update and delete

--- server/src/controllers/interviewSession.controller.ts
+++ server/src/controllers/interviewSession.controller.ts
@@ -1,21 +1,65 @@
 import catchAsync from "../utils/catchAsync";
+import {CREATED, OK} from "../constants/http";
+import {createInterviewSessionSchema, updateInterviewSessionSchema} from "../validations/interviewSession.schemas";
+import {
+    createInterviewSessionService, deleteInterviewSessionService,
+    getAllInterviewSessionsService,
+    getInterviewSessionByIdService, updateInterviewSessionService
+} from "../services/interviewSession.service";
+import {ensureFound, toObjectId} from "../utils/dataHelpers";
 
 export const getAllInterviewSessions = catchAsync(async (req, res, next) => {
-    res.send('all ISs')
+    const sessions = await getAllInterviewSessionsService(req.userId)
+
+    return res.status(OK).json({
+        status: 'success',
+        sessions
+    })
 })
 
 export const createInterviewSession = catchAsync(async (req, res, next) => {
-    res.send('create IS')
+    const data = createInterviewSessionSchema.parse(req.body)
+
+    const session = await createInterviewSessionService(req.userId, data)
+
+    return res.status(CREATED).json({
+        status: 'success',
+        session
+    })
 })
 
 export const getInterviewSessionById = catchAsync(async (req, res, next) => {
-    res.send('get IS')
+    const sessionId = toObjectId(req.params.id)
+
+    const session = await getInterviewSessionByIdService(req.userId, sessionId)
+    ensureFound(session, 'Interview Session Not Found')
+
+    return res.status(OK).json({
+        status: 'success',
+        session
+    })
 })
 
 export const updateInterviewSession = catchAsync(async (req, res, next) => {
-    res.send('update IS')
+    const sessionId = toObjectId(req.params.id)
+    const data = updateInterviewSessionSchema.parse(req.body)
+
+    const updatedInterviewSession = await updateInterviewSessionService(req.userId, sessionId, data)
+    ensureFound(updatedInterviewSession, 'Interview Session Not Found')
+
+    return res.status(OK).json({
+        status: 'success',
+        session: updatedInterviewSession
+    })
 })
 
 export const deleteInterviewSession = catchAsync(async (req, res, next) => {
-    res.send('delete IS')
+    const sessionId = toObjectId(req.params.id);
+
+    await deleteInterviewSessionService(req.userId, sessionId)
+
+    return res.status(OK).json({
+        status: 'success',
+        message: 'Interview Session Deleted Successfully'
+    })
 })

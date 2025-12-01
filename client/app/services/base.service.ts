5c882680622775f265c265d5aeb9623e5a8b814b
Nikola Kenjic <nikola.nico89@gmail.com>
2025-12-01 20:19:19 +0100

    feat(base-service): add patch method and support config for all requests

--- client/app/services/base.service.ts
+++ client/app/services/base.service.ts
@@ -1,19 +1,23 @@
 import ApiClient from "./api-client/api-client";
 
 export default class BaseService {
-    static async create<T>(endpoint: string, body: any, queryParams = {}): Promise<T> {
-        return ApiClient.post(endpoint, body, queryParams).then(res => res.data);
-    }
-
     static async fetch<T>(endpoint: string, queryParams = {}, config = {}): Promise<T> {
         return ApiClient.get(endpoint, queryParams, config).then(res => res.data);
     }
 
-    static async delete<T>(endpoint: string, queryParams = {}): Promise<T> {
-        return ApiClient.remove(endpoint, queryParams).then(res => res.data);
+    static async create<T>(endpoint: string, body: any, queryParams = {}, config = {}): Promise<T> {
+        return ApiClient.post(endpoint, body, queryParams, config).then(res => res.data);
     }
 
-    static async update<T>(endpoint: string, body: any, queryParams = {}): Promise<T> {
-        return ApiClient.put(endpoint, body, queryParams).then(res => res.data);
+    static async patch<T>(endpoint: string, body: any, queryParams = {}, config = {}): Promise<T> {
+        return ApiClient.patch(endpoint, body, queryParams, config).then(res => res.data);
+    }
+
+    static async update<T>(endpoint: string, body: any, queryParams = {}, config = {}): Promise<T> {
+        return ApiClient.put(endpoint, body, queryParams, config).then(res => res.data);
+    }
+
+    static async delete<T>(endpoint: string, queryParams = {}, config = {}): Promise<T> {
+        return ApiClient.remove(endpoint, queryParams, config).then(res => res.data);
     }
 }

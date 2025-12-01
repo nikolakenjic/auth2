3b12cabd2c6f6f73b8b211d152091657f1bca5b7
Nikola Kenjic <nikola.nico89@gmail.com>
2025-12-01 20:19:47 +0100

    feat(api-client): add patch method and support config for all requests

--- client/app/services/api-client/api-client.ts
+++ client/app/services/api-client/api-client.ts
@@ -6,16 +6,20 @@ export default class ApiClient {
         return axiosInstance.get(endpoint, {params: queryParams, ...config});
     }
 
-    static async post(endpoint: string, body: any, queryParams = {}): Promise<AxiosResponse> {
-        return axiosInstance.post(endpoint, body, {params: queryParams});
+    static async post(endpoint: string, body: any, queryParams = {}, config = {}): Promise<AxiosResponse> {
+        return axiosInstance.post(endpoint, body, {params: queryParams, ...config});
     }
 
-    static async put(endpoint: string, body: any, queryParams = {}): Promise<AxiosResponse> {
-        return axiosInstance.put(endpoint, body, {params: queryParams});
+    static async put(endpoint: string, body: any, queryParams = {}, config = {}): Promise<AxiosResponse> {
+        return axiosInstance.put(endpoint, body, {params: queryParams, ...config});
     }
 
-    static async remove(endpoint: string, queryParams: any = {}): Promise<AxiosResponse> {
-        return axiosInstance.delete(endpoint, {params: queryParams});
+    static async patch(endpoint: string, body: any, queryParams = {}, config = {}): Promise<AxiosResponse> {
+        return axiosInstance.patch(endpoint, body, {params: queryParams, ...config});
+    }
+
+    static async remove(endpoint: string, queryParams = {}, config = {}): Promise<AxiosResponse> {
+        return axiosInstance.delete(endpoint, {params: queryParams, ...config});
     }
 }
 

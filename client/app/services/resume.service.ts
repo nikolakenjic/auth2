cf0e76f369fc6837caed7e7672fccb7bcf57d348
Nikola Kenjic <nikola.nico89@gmail.com>
2025-12-03 23:40:02 +0100

    refactor(resume-service): improve types and replace any with Resume interfaces

--- client/app/services/resume.service.ts
+++ client/app/services/resume.service.ts
@@ -1,22 +1,23 @@
 import BaseService from "@/app/services/base.service";
+import {Resume, ResumeListResponse, ResumeResponse} from "@/app/types/resume.types";
 
 export default class ResumeService {
     static readonly ENDPOINT = "/resume";
 
     static async getAll() {
-        return BaseService.fetch<{ status: string; resumes: any[] }>(this.ENDPOINT)
+        return BaseService.fetch<ResumeListResponse>(this.ENDPOINT)
     }
 
     static async getById(id: string) {
-        return BaseService.fetch<{ status: string; resume: any }>(`${this.ENDPOINT}/${id}`);
+        return BaseService.fetch<ResumeResponse>(`${this.ENDPOINT}/${id}`);
     }
 
-    static async create(data: any) {
+    static async create(data: Partial<Resume>) {
         return BaseService.create<{ status: string; resume: any }>(this.ENDPOINT, data);
     }
 
-    static async update(id: string, data: any) {
-        return BaseService.patch<{ status: string; resume: any }>(`${this.ENDPOINT}/${id}`, data);
+    static async update(id: string, data: Partial<Resume>) {
+        return BaseService.patch<ResumeResponse>(`${this.ENDPOINT}/${id}`, data);
     }
 
     static async remove(id: string) {

8363cd86c0ce7791534df612b9986309f6b224d2
Nikola Kenjic <nikola.nico89@gmail.com>
2025-12-14 23:42:01 +0100

    refactor(validate): add appError to validate

--- server/src/middleware/validate.ts
+++ server/src/middleware/validate.ts
@@ -1,5 +1,8 @@
 import {AnyZodObject, ZodError} from "zod";
-import {Request, Response, NextFunction} from "express";
+import {NextFunction, Request, Response} from "express";
+import AppError from "../utils/AppError";
+import {BAD_REQUEST} from "../constants/http";
+import AppErrorCode from "../constants/appErrorCode";
 
 export const validate =
     (schema: AnyZodObject) =>
@@ -10,16 +13,26 @@ export const validate =
                 next();
             } catch (err: any) {
                 if (err instanceof ZodError) {
-                    return res.status(400).json({
-                        status: "error",
-                        message: err.errors?.[0]?.message,
-                        errors: err.errors
-                    });
+                    const formatted = err.issues.map((issue) => ({
+                        path: issue.path.join('.'),
+                        message: issue.message,
+                    }));
+                    return next(
+                        new AppError(
+                            BAD_REQUEST,
+                            'Validation failed',
+                            AppErrorCode.ValidationError,
+                            {errors: formatted}
+                        )
+                    );
                 }
 
-                return res.status(500).json({
-                    status: "error",
-                    message: 'Validation failed.'
-                })
+                return next(
+                    new AppError(
+                        BAD_REQUEST,
+                        "Validation failed",
+                        AppErrorCode.ValidationError,
+                    )
+                );
             }
         };

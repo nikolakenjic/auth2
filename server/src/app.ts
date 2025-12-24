970014740a292ae5df3888ab6baa712e7c8ece9c
Nikola Kenjic <nikola.nico89@gmail.com>
2025-12-24 22:07:11 +0100

    refactor(app): refactor app

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,41 +1,13 @@
 import express from 'express';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
 import errorHandler from './middleware/errorHandler';
-import catchAsync from './utils/catchAsync';
-import authRoute from './routes/auth.route';
-import authenticate from './middleware/authenticate';
-import userRoute from './routes/user.route';
-import sessionRoute from './routes/session.route';
-import resumeRoute from './routes/resume.route';
-import coverLetterRoute from "./routes/coverLetter.route";
-import interviewSessionRoute from "./routes/interviewSession.route";
+import apiRoutes from './routes'
+import {coreMiddleware} from "./middleware";
 
 const app = express();
 
-app.use(cookieParser());
-app.use(express.json());
-app.use(cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-}))
+coreMiddleware(app)
 
-app.get(
-    '/',
-    catchAsync(async (req, res, next) => {
-        // throw new Error('This is an error');
-        return res.send('Node auth');
-    })
-);
-
-app.use('/auth', authRoute);
-
-// protected routes
-app.use('/user', authenticate, userRoute);
-app.use('/sessions', authenticate, sessionRoute);
-app.use('/resume', authenticate, resumeRoute)
-app.use('/cover-letter', authenticate, coverLetterRoute)
-app.use('/interview-session', authenticate, interviewSessionRoute)
+app.use('/api/v1', apiRoutes)
 
 // Error
 app.use(errorHandler);

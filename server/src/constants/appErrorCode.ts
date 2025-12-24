138b6ef1a182412a6e6bbbf3c78fc2b9205f6794
Nikola Kenjic <nikola.nico89@gmail.com>
2025-12-24 22:17:02 +0100

    refactor(app-error): add some error detail to my errors

--- server/src/constants/appErrorCode.ts
+++ server/src/constants/appErrorCode.ts
@@ -1,5 +1,30 @@
 const enum AppErrorCode {
+    // Auth
     InvalidAccessToken = 'InvalidAccessToken',
+    TokenExpired = 'TokenExpired',
+    Unauthorized = 'Unauthorized',
+    Forbidden = 'Forbidden',
+
+    // Validation
+    ValidationError = 'ValidationError',
+
+    // User / Session
+    UserNotFound = 'UserNotFound',
+    SessionExpired = 'SessionExpired',
+    SessionNotFound = 'SessionNotFound',
+
+    // Resume
+    ResumeNotFound = 'ResumeNotFound',
+    ResumeLimitReached = 'ResumeLimitReached',
+
+    // Cover Letter
+    CoverLetterNotFound = 'CoverLetterNotFound',
+
+    // Interview
+    InterviewSessionNotFound = 'InterviewSessionNotFound',
+
+    // Generic
+    InternalServerError = 'InternalServerError',
 }
 
 export default AppErrorCode;
--- server/src/utils/AppError.ts
+++ server/src/utils/AppError.ts
@@ -2,12 +2,23 @@ import {HttpStatusCode} from "../constants/http";
 import AppErrorCode from "../constants/appErrorCode";
 
 class AppError extends Error {
+    public statusCode: HttpStatusCode;
+    public errorCode?: AppErrorCode;
+    public details?: unknown;
+
     constructor(
-        public statusCode: HttpStatusCode,
-        public message: string,
-        public errorCode?: AppErrorCode
+        statusCode: HttpStatusCode,
+        message: string,
+        errorCode?: AppErrorCode,
+        details?: unknown
     ) {
         super(message)
+
+        this.statusCode = statusCode;
+        this.errorCode = errorCode;
+        this.details = details;
+
+        Object.setPrototypeOf(this, AppError.prototype);
     }
 }
 

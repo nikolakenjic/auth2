ce6cd0f2b02f5506cd2cdba74f3b6aef5c8ecabe
Nikola Kenjic <nikola.nico89@gmail.com>
2025-12-27 19:41:02 +0100

    feat(authenticate): add session model to authenticate

--- server/src/middleware/authenticate.ts
+++ server/src/middleware/authenticate.ts
@@ -4,8 +4,9 @@ import {UNAUTHORIZED} from '../constants/http';
 import AppErrorCode from '../constants/appErrorCode';
 import {verifyToken} from '../utils/jwt';
 import {ObjectId} from "mongodb";
+import SessionModel from "../models/session.model";
 
-const authenticate: RequestHandler = (req, res, next) => {
+const authenticate: RequestHandler = async (req, res, next) => {
     const authHeader = req.headers.authorization;
     const tokenFromHeader = authHeader?.startsWith("Bearer ") ? authHeader.split(" ")[1] : undefined;
     const tokenFromCookie = req.cookies.accessToken as string;
@@ -26,10 +27,19 @@ const authenticate: RequestHandler = (req, res, next) => {
         AppErrorCode.InvalidAccessToken
     );
 
+    // 🔒 SESSION CHECK
+    const session = await SessionModel.findById(payload.sessionId);
+    appAssert(
+        session,
+        UNAUTHORIZED,
+        'Session expired',
+        AppErrorCode.InvalidAccessToken
+    );
+
     // attach user object
     req.user = {
         userId: new ObjectId(payload.userId as string),
-        role: payload.role || 'user',
+        role: payload.role ?? 'user',
         sessionId: new ObjectId(payload.sessionId as string)
     };
 

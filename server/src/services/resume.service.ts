e7ff8c54b42d67e71b6c7e511e61ee3256c374b8
Nikola Kenjic <nikola.nico89@gmail.com>
2026-01-18 22:15:19 +0100

    refactor(resume-service): refactor resume service

--- server/src/services/resume.service.ts
+++ server/src/services/resume.service.ts
@@ -1,22 +1,35 @@
 import mongoose from "mongoose";
 import ResumeModel from "../models/resume.model";
+import {CreateResumeInput, UpdateResumeInput} from "../validations/resume.schemas";
 
-export const getAllResumesService = async (userId: mongoose.Types.ObjectId) => {
-    return await ResumeModel.find({userId})
+export const getAllResumesService = async (
+    userId: mongoose.Types.ObjectId
+) => {
+    return await ResumeModel.find({userId}).sort({createdAt: -1})
 }
 
-export const createResumeService = async (userId: mongoose.Types.ObjectId, data: any) => {
+export const createResumeService = async (
+    userId: mongoose.Types.ObjectId,
+    data: CreateResumeInput
+) => {
     return await ResumeModel.create({
         ...data,
         userId
     })
 }
 
-export const getResumeByIdService = async (userId: mongoose.Types.ObjectId, resumeId: mongoose.Types.ObjectId) => {
+export const getResumeByIdService = async (
+    userId: mongoose.Types.ObjectId,
+    resumeId: mongoose.Types.ObjectId
+) => {
     return await ResumeModel.findOne({_id: resumeId, userId})
 }
 
-export const updateResumeService = async (userId: mongoose.Types.ObjectId, resumeId: mongoose.Types.ObjectId, data: any) => {
+export const updateResumeService = async (
+    userId: mongoose.Types.ObjectId,
+    resumeId: mongoose.Types.ObjectId,
+    data: UpdateResumeInput
+) => {
     return await ResumeModel.findOneAndUpdate(
         {_id: resumeId, userId},
         {$set: data},
@@ -24,6 +37,9 @@ export const updateResumeService = async (userId: mongoose.Types.ObjectId, resum
     )
 }
 
-export const deleteResumeService = async (userId: mongoose.Types.ObjectId, resumeId: mongoose.Types.ObjectId) => {
+export const deleteResumeService = async (
+    userId: mongoose.Types.ObjectId,
+    resumeId: mongoose.Types.ObjectId
+) => {
     return await ResumeModel.findOneAndDelete({_id: resumeId, userId})
 }

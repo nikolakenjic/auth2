4e29c9e821500000e2ba6634dac7bfbab47d728c
Nikola Kenjic <nikola.nico89@gmail.com>
2026-01-23 22:51:26 +0100

    refactor(auth): refactor auth code

--- client/app/services/api-client/axios-instance.ts
+++ client/app/services/api-client/axios-instance.ts
@@ -6,7 +6,7 @@ interface AxiosRequestConfigWithRetry extends AxiosRequestConfig {
 }
 
 const axiosInstance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3500",
+    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000/api/v1",
     headers: {
         "Content-Type": "application/json",
     },
@@ -19,15 +19,21 @@ axiosInstance.interceptors.response.use(
     async (error) => {
         const originalRequest = error.config as AxiosRequestConfigWithRetry;
 
-    //     If token expires and we do not try refresh
-        if(error.response?.status === 401 && !originalRequest._retry) {
+        const isAuthEndpoint =
+            originalRequest.url?.includes("/auth/refresh") ||
+            originalRequest.url?.includes("/auth/login") ||
+            originalRequest.url?.includes("/auth/register") ||
+            originalRequest.url?.includes("/auth/logout")
+
+        //     If token expires and we do not try refresh
+        if (error.response?.status === 401 && !originalRequest._retry && !isAuthEndpoint) {
             originalRequest._retry = true;
 
             try {
-            //     Call backend to refresh accessToken
+                //     Call backend to refresh accessToken
                 await AuthService.refresh()
 
-            //     send again original request
+                //     send again original request
                 return axiosInstance(originalRequest)
             } catch (refreshError) {
                 console.error('RefreshToken Invalid or Expired')

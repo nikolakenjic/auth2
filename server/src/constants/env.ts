701bc00c4fbb67b013acc4d017b30d271673aec8
Nikola Kenjic <nikola.nico89@gmail.com>
2026-04-01 00:02:07 +0200

    install groq dependencies

--- server/src/constants/env.ts
+++ server/src/constants/env.ts
@@ -18,6 +18,7 @@ export const JWT_SECRET = getEnv('JWT_SECRET');
 export const JWT_REFRESH_SECRET = getEnv('JWT_REFRESH_SECRET');
 export const EMAIL_SENDER = getEnv('EMAIL_SENDER');
 export const RESEND_API_KEY = getEnv('RESEND_API_KEY');
+export const GROQ_API_KEY = getEnv('GROQ_API_KEY');
 
 export const ENV = {
     DEVELOPMENT: 'development',
